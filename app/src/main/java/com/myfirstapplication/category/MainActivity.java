bde1a0f05363d18069a3cc42ec08277e7d23c536
IT20011048 <it20011048@my.sliit.lk>
2021-09-24 17:32:12 +0530

    message create and read part

--- app/src/main/java/com/myfirstapplication/category/MainActivity.java
+++ app/src/main/java/com/myfirstapplication/category/MainActivity.java
@@ -1,37 +1,34 @@
 package com.myfirstapplication.category;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.widget.Button;
+
 import android.content.Intent;
-import android.view.View;
-
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import com.myfirstapplication.category.all.message;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
-
     public Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-               Intent intent = new Intent(MainActivity.this, message.class);
-               startActivity(intent);
-
-            }
-
-
-        });
+      button = (Button)findViewById(R.id.button);
+      button.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              Intent intent = new Intent(MainActivity.this,messagelist.class);
+              startActivity(intent);
+          }
+      });
 
     }
+
+
+
 }
---
+++ app/src/main/java/com/myfirstapplication/category/message.java
@@ -0,0 +1,77 @@
+package com.myfirstapplication.category;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+import com.myfirstapplication.category.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class message extends AppCompatActivity {
+
+    EditText email,subject,message;
+    Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.message);
+
+
+       email =(EditText) findViewById(R.id.email);
+       subject =(EditText) findViewById(R.id.subject);
+       message =(EditText) findViewById(R.id.message);
+
+       button2 = (Button) findViewById(R.id.button2);
+
+       button2.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               inserData();
+               clearall();
+           }
+       });
+
+
+
+
+    }
+    private void inserData(){
+        Map<String,Object> map = new HashMap<>();
+        map.put("email",email.getText().toString());
+        map.put("subject",subject.getText().toString());
+        map.put("message",message.getText().toString());
+
+        FirebaseDatabase.getInstance().getReference().child("customer").push()
+                .setValue(map)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(message.this,"data inserted",Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(message.this, "insertion failed",Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void clearall(){
+
+        email.setText("");
+        subject.setText("");
+        message.setText("");
+    }
+}
